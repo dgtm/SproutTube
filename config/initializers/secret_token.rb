e807e6aab58fbd3be01e99ade890b8abe41be45f
flame <kshitiz.gurung@sprout-technology.com>
2011-04-05 13:31:36 +0545

    Basic Mockup Created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SproutTube::Application.config.secret_token = '6fbb99616ec0f3fa3548d223dd759e9b8f4ba85a7d7c37cfdb94929bb0d93fcc5869c49e2d10f1e70f3de7343f1d33d4dd0ff644812d132207fe632d502ffaa2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SproutTube::Application.config.session_store :cookie_store, :key => '_SproutTube_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SproutTube::Application.config.session_store :active_record_store
